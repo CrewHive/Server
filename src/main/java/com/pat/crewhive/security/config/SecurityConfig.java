c5d61c6c937322bf2e2c16aeb588170abe6ec735
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2026-09-11 15:13:16 +0200

    Logback, L3 e L4

--- src/main/java/com/pat/crewhive/security/config/SecurityConfig.java
+++ src/main/java/com/pat/crewhive/security/config/SecurityConfig.java
@@ -67,8 +67,7 @@ public class SecurityConfig {
                         .requestMatchers(POST, "/api/auth/register").permitAll()
                         .requestMatchers(POST, "/api/auth/register/manager").permitAll()
                         .requestMatchers(POST, "/api/auth/rotate").permitAll()
-                        .requestMatchers("/docs", "/docs/**",
-                                         "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/docs", "/docs/**").hasRole("DEV")
                         .anyRequest().authenticated()
                 )
 
@@ -115,7 +114,8 @@ public class SecurityConfig {
         CorsConfiguration cfg = new CorsConfiguration();
 
         cfg.setAllowCredentials(true);
-        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "QUERY", "PATCH", "DELETE", "OPTIONS"));
+        cfg.setAllowedOrigins(List.of("*"));
+        cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         cfg.setExposedHeaders(List.of("Authorization")); // Per il token JWT
         // Preflight cache
